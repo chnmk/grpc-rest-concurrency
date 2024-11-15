7661a4f599b8e8ae610c6ee77606d49e3d4061b9
chnmk <a2molchanov@ya.ru>
2024-11-15 12:34:31 +0300

    fix: wait for the servers to start

--- grpc/client.go
+++ grpc/client.go
@@ -6,24 +6,13 @@ import (
 	"time"
 
 	pb "github.com/chnmk/grpc-rest-concurrency/grpc/example"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
-func Client() {
-	// May not work properly with "localhost" address
-	conn, err := grpc.NewClient("127.0.0.1:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-
-	defer conn.Close()
-	c := pb.NewGreeterClient(conn)
-
+func Client(c pb.GreeterClient) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	_, err = c.SayHello(ctx, &pb.HelloRequest{Name: "World"})
+	_, err := c.SayHello(ctx, &pb.HelloRequest{Name: "World"})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
--- main.go
+++ main.go
@@ -2,17 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"sync"
+	"time"
 
 	"github.com/chnmk/grpc-rest-concurrency/counter"
 	grpc_api "github.com/chnmk/grpc-rest-concurrency/grpc"
+	pb "github.com/chnmk/grpc-rest-concurrency/grpc/example"
 	rest_api "github.com/chnmk/grpc-rest-concurrency/rest"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
+	// Start the servers in goroutines
 	go rest_api.Server()
 	go grpc_api.Server()
 
+	// Create a gRPC client
+	conn, err := grpc.NewClient("127.0.0.1:50051", // May not work properly with "localhost" address
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Wait for the servers to start
+	time.Sleep(time.Second * 5)
+
+	// Create clients to read signals from a channel
 	var wg sync.WaitGroup
 
 	ch := make(chan int, 5)
@@ -29,10 +49,11 @@ func main() {
 	go func() {
 		defer wg.Done()
 		for range ch {
-			grpc_api.Client()
+			grpc_api.Client(c)
 		}
 	}()
 
+	// Send signals to a channel
 	for i := 0; i < 50; i++ {
 		ch <- i
 	}
@@ -41,6 +62,7 @@ func main() {
 
 	wg.Wait()
 
+	// Check which technology handled more signals
 	fmt.Println("REST: ", counter.Counter_REST)
 	fmt.Println("gRPC: ", counter.Counter_gRPC)
 }
